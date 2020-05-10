5d141a1aee5e91a97f3d77ada4be90b719876a2a
basemj <basem.jawahri@gmail.com>
2020-05-10 18:00:11 +0100

    init

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+      <Text>Open up App.js to start working on your app?</Text>
     </View>
   );
 }
@@ -12,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#bada55",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
