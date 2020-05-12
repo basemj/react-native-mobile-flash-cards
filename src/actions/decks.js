1219d92ea3704a68fe180252e1f7e10e53c8d532
basemj <basem.jawahri@gmail.com>
2020-05-12 02:27:50 +0100

    feat: redux actions

--- src/actions/decks.js
+++ src/actions/decks.js
@@ -1,4 +1,10 @@
-import data from "../utils/data";
+import {
+  _addCardToDeck,
+  _addDeck,
+  _deleteDeck,
+  _getAllDecks,
+  _getDeck,
+} from "../utils/data";
 
 const ADD_CARD_TO_DECK = "ADD_CARD_TO_DECK";
 const ADD_DECK = "ADD_DECK";
@@ -8,9 +14,8 @@ const GET_DECK = "GET_DECK";
 
 const handleAddCardToDeck = (deckId, questionSet) => {
   return (dispatch) => {
-    return data
-      .addCardToDeck(deckId, questionSet)
-      .then(({ deckId, questionSet }) => {
+    return _addCardToDeck(deckId, questionSet)
+      .then(() => {
         return dispatch(addCardToDeck(deckId, questionSet));
       })
       .catch((error) => {
@@ -27,21 +32,71 @@ const addCardToDeck = (deckId, questionSet) => {
   };
 };
 
-const handleAddDeck = () => {};
+const handleAddDeck = (title) => {
+  return (dispatch) => {
+    return _addDeck(title).then(() => {
+      return dispatch(addDeck(title));
+    });
+  };
+};
 
-const addDeck = () => {};
+const addDeck = (title) => {
+  return {
+    key: ADD_DECK,
+    title,
+  };
+};
 
-const handleDeleteDeck = () => {};
+const handleDeleteDeck = (deckId) => {
+  return (dispatch) => {
+    return _deleteDeck(deckId)
+      .then(() => {
+        return dispatch(deleteDeck(deckId));
+      })
+      .catch((error) => {
+        return error;
+      });
+  };
+};
 
-const deleteDeck = () => {};
+const deleteDeck = (deckId) => {
+  return {
+    type: DELETE_DECK,
+    deckId,
+  };
+};
 
-const handleGetAllDecks = () => {};
+const handleGetAllDecks = () => {
+  return (dispatch) => {
+    _getAllDecks()
+      .then((decks) => dispatch(getAllDecks(decks)))
+      .catch((error) => {
+        return error;
+      });
+  };
+};
 
-const getAllDecks = () => {};
+const getAllDecks = (decks) => {
+  return {
+    type: GET_ALL_DECKS,
+    decks,
+  };
+};
 
-const handleGetDeck = () => {};
+const handleGetDeck = (deckId) => {
+  return (dispatch) => {
+    return _getDeck(deckId).then(() => {
+      return dispatch(getDeck(deckId));
+    });
+  };
+};
 
-const getDeck = () => {};
+const getDeck = (deckId) => {
+  return {
+    key: GET_DECK,
+    deckId,
+  };
+};
 
 export {
   ADD_CARD_TO_DECK,
