913cd9d8c48af1777c64a397da02dbd25b4c5618
basemj <basem.jawahri@gmail.com>
2020-05-13 05:39:18 +0100

    refactor: empty initial state

--- src/reducers/decks.js
+++ src/reducers/decks.js
@@ -5,35 +5,7 @@ import {
   GET_ALL_DECKS,
 } from "../actions/decks";
 
-const initState = {
-  react: {
-    id: "react",
-    title: "React",
-    questions: [
-      {
-        question: "What is React?",
-        answer: "A library for managing user interfaces",
-      },
-      {
-        question: "Where do you make Ajax requests in React?",
-        answer: "The componentDidMount lifecycle event",
-      },
-    ],
-  },
-  javascript: {
-    id: "javascript",
-    title: "JavaScript",
-    questions: [
-      {
-        question: "What is a closure?",
-        answer:
-          "The combination of a function and the lexical environment within which that function was declared.",
-      },
-    ],
-  },
-};
-
-const decks = (state = initState, action) => {
+const decks = (state = {}, action) => {
   switch (action.type) {
     case ADD_CARD_TO_DECK: {
       const { deckId, questionSet } = action;
